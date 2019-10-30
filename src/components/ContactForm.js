8517a2347471eef3fd7d55abda64cedfe6f986dc
Michael Liendo <mtliendo@gmail.com>
2019-10-29 21:54:17 -0500

    finish frontend with controlled state

--- src/components/ContactForm.js
+++ src/components/ContactForm.js
@@ -1,32 +1,86 @@
-import React from 'react'
+import React, { useReducer } from 'react'
 import styles from './contactform.module.css'
 
+const INITIAL_STATE = {
+  fullname: '',
+  email: '',
+  subject: '',
+  body: ''
+}
+
+const reducer = (state, action) => {
+  switch (action.type) {
+    case 'updateFormField':
+      return {
+        ...state,
+        [action.fieldName]: action.fieldValue
+      }
+    default:
+      return INITIAL_STATE
+  }
+}
 const ContactForm = () => {
+  const [state, dispatch] = useReducer(reducer, INITIAL_STATE)
+
+  const handleInputChange = fieldName => event => {
+    dispatch({
+      type: 'updateFormField',
+      fieldName,
+      fieldValue: event.target.value
+    })
+  }
+
+  const handleFormSubmit = e => {
+    e.preventDefault()
+    console.log(state)
+  }
   return (
-    <form className={`${styles.form}`}>
+    <form className={`${styles.form}`} onSubmit={handleFormSubmit}>
       <label htmlFor="fullname" className={`${styles.label}`}>
         Full Name
       </label>
-      <input id="fullname" className={`${styles.input}`} type="text" />
+      <input
+        value={state.fullname}
+        onChange={handleInputChange('fullname')}
+        id="fullname"
+        className={`${styles.input}`}
+        type="text"
+      />
 
       <label htmlFor="email" className={`${styles.label}`}>
         Email
       </label>
-      <input className={`${styles.input}`} type="email" id="email" />
+      <input
+        value={state.email}
+        onChange={handleInputChange('email')}
+        className={`${styles.input}`}
+        type="email"
+        id="email"
+      />
 
       <label htmlFor="subject" className={`${styles.label}`}>
         Subject
       </label>
-      <input className={`${styles.input}`} type="text" id="subject" />
+      <input
+        value={state.subject}
+        onChange={handleInputChange('subject')}
+        className={`${styles.input}`}
+        type="text"
+        id="subject"
+      />
 
       <label htmlFor="body" className={`${styles.label}`}>
         Message
       </label>
-      <textarea className={`${styles.input}`} type="text" id="body" />
+      <textarea
+        value={state.body}
+        onChange={handleInputChange('body')}
+        className={`${styles.input}`}
+        type="text"
+        id="body"
+      />
 
-      <button className={`${styles.button}`} type="reset">
-        Submit
-      </button>
+      <button className={`${styles.button}`}>Submit</button>
     </form>
   )
 }
